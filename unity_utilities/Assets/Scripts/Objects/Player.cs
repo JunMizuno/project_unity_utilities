2c6cbe29144b806b36c7cdd40f21b040a626e524
JUNMIZUNO_PAV\gvist <create.gamescene@gmail.com>
2026-05-03 17:03:24 +0900

    デモ用に調整

--- unity_utilities/Assets/Scripts/Objects/Player.cs
+++ unity_utilities/Assets/Scripts/Objects/Player.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using R3;
 
 public class Player : MonoBehaviour
 {
@@ -7,8 +9,13 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-        rigidBoby.useGravity = true;
-        rigidBoby.AddForce(Vector3.forward * 12.0f, ForceMode.Impulse);
+        Observable.Timer(TimeSpan.FromSeconds(1))
+            .Subscribe(_ =>
+            {
+                rigidBoby.useGravity = true;
+                rigidBoby.AddForce(Vector3.forward * 12.0f, ForceMode.Impulse);
+            })
+            .AddTo(this);
     }
 
     void Update()
